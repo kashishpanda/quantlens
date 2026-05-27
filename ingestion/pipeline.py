a94eb2c926a1865b8587fcac13689cdd9e9fa33c
kashishpanda <kashishpanda@gmail.com>
2026-05-26 23:53:18 -0400

    feat: upgrade pipeline to fetch all stocks automatically in one run

--- ingestion/pipeline.py
+++ ingestion/pipeline.py
@@ -9,28 +9,23 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
-symbol = input("Enter stock symbol (e.g AAPL)")
+symbols = ["AAPL", "GOOGL", "MSFT", "NVDA", "JPM", "AMZN"]
 period = "1mo"
-ticker = yf.Ticker(symbol)
-data = ticker.history(period = period)
+for symbol in symbols:
+    ticker = yf.Ticker(symbol)
+    data = ticker.history(period = period)
+    data = data[[ "Open", "High", "Low", "Close", "Volume"]]
+    data = data.reset_index()
+    data.rename(columns = {"Date" : "date"}, inplace =True)
+    data["date"] = pd.to_datetime(data["date"]).dt.date
+    data["Volume"] = data["Volume"].astype(int)
+    data["Ticker"] = symbol
+    data.columns = data.columns.str.lower()
+    data.to_sql(         # Insert data to database
+        "equity_prices",
+        engine,
+        if_exists = 'append',
+        index = False
+    )
 
-data = data[[ "Open", "High", "Low", "Close", "Volume"]]
-data = data.reset_index()
-data.rename(columns = {"Date" : "date"}, inplace =True)
-data["date"] = pd.to_datetime(data["date"]).dt.date
-data["Volume"] = data["Volume"].astype(int)
-data["Ticker"] = symbol
-data.columns = data.columns.str.lower()
-
-
-print(data)
-
-# Insert data to database
-data.to_sql( 
-    "equity_prices",
-    engine,
-    if_exists = 'append',
-    index = False
-)
-
-print('Data inserted successfully')
+    print(f"{symbol} inserted successfully")
