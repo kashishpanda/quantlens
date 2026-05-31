6772463bc2c5c481d5e4c782d76c83c3b1b70ecd
kashishpanda <kashishpanda@gmail.com>
2026-05-30 23:23:42 -0400

    feat: add error handling and empty data check to pipeline

--- ingestion/pipeline.py
+++ ingestion/pipeline.py
@@ -16,30 +16,37 @@ with engine.connect() as conn:
 print(f"Fetching data for: {symbols}")
 period = "1mo"
 for symbol in symbols:
-    ticker = yf.Ticker(symbol)
-    data = ticker.history(period = period)
-    data = data[[ "Open", "High", "Low", "Close", "Volume"]]
-    data = data.reset_index()
-    data.rename(columns = {"Date" : "date"}, inplace =True)
-    data["date"] = pd.to_datetime(data["date"]).dt.date
-    data["Volume"] = data["Volume"].astype(int)
-    data["Ticker"] = symbol
-    data.columns = data.columns.str.lower()
-    with engine.connect() as conn:
-        for _, row in data.iterrows():
-            conn.execute(text("""
-                INSERT INTO equity_prices (date, open, high, low, close, volume, ticker)
-                VALUES (:date, :open, :high, :low, :close, :volume, :ticker)
-                ON CONFLICT (date, ticker) DO NOTHING
-            """), {
-                "date": row["date"],
-                "open": row["open"],
-                "high": row["high"],
-                "low": row["low"],
-                "close": row["close"],
-                "volume": row["volume"],
-                "ticker": row["ticker"]
-            })
-        conn.commit()  
-
-    print(f"{symbol} inserted successfully")
+    try:
+            ticker = yf.Ticker(symbol)
+            data = ticker.history(period = period)
+            if data.empty:
+                print(f"WARNING - No data found for {symbol}, skipping")
+                continue
+            data = data[[ "Open", "High", "Low", "Close", "Volume"]]
+            data = data.reset_index()
+            data.rename(columns = {"Date" : "date"}, inplace =True)
+            data["date"] = pd.to_datetime(data["date"]).dt.date
+            data["Volume"] = data["Volume"].astype(int)
+            data["Ticker"] = symbol
+            data.columns = data.columns.str.lower()
+            with engine.connect() as conn:
+                for _, row in data.iterrows():
+                    conn.execute(text("""
+                        INSERT INTO equity_prices (date, open, high, low, close, volume, ticker)
+                        VALUES (:date, :open, :high, :low, :close, :volume, :ticker)
+                        ON CONFLICT (date, ticker) DO NOTHING
+                    """), {
+                        "date": row["date"],
+                        "open": row["open"],
+                        "high": row["high"],
+                        "low": row["low"],
+                        "close": row["close"],
+                        "volume": row["volume"],
+                        "ticker": row["ticker"]
+                    })
+                conn.commit()  
+            print(f"{symbol} inserted successfully")
+    except Exception as e:
+        print(f"ERROR - {symbol} failed: {e}")                
+    
+    
